a18f0285cc1db41342b1451d2c901937890e70af
adam <gog172172@gmail.com>
2020-06-08 16:06:57 +0200

    setup Database connection

---
+++ src/main/java/com/example/mongoDB/entities/Film.java
@@ -0,0 +1,30 @@
+package com.example.mongoDB.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Film {
+
+    @Id
+    public String id;
+    public String title;
+    public Integer year;
+    public String category;
+
+    public Film(String title, Integer year, String category) {
+        this.title = title;
+        this.year = year;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
