021a27886d2272dcd8be583744a43263178b239d
adam <gog172172@gmail.com>
2020-06-10 22:38:04 +0200

    delete film

--- src/main/java/com/example/mongoDB/FilmController.java
+++ src/main/java/com/example/mongoDB/FilmController.java
@@ -5,11 +5,12 @@ import com.example.mongoDB.repositiory.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class FilmController {
@@ -26,15 +27,27 @@ public class FilmController {
             @RequestParam(value = "year", required = false) Integer year,
             @RequestParam(value = "category", required = false) String category
     ) {
-        if (title != null && year != null && category != null){
-            repository.insert(new Film(title,year,category));
+        if (title != null && year != null && category != null) {
+            repository.insert(new Film(title, year, category));
         }
         return "index";
     }
 
-    @GetMapping
+
     @RequestMapping("/showFindByCategory")
-    public String showFindByCategory(Model model) {
+    public String showFindByCategory(
+            Model model) {
+        List<Film> showFindByCategory = repository.findByCategory("Drama");
+        model.addAttribute("showFindByCategory", showFindByCategory);
+        return "showFindByCategory";
+    }
+
+    @RequestMapping("/showFindByCategory/{id}")
+    public String deleteElement(
+            @PathVariable(value = "id") String id,
+            Model model) {
+        Optional<Film> filmToDelete = repository.findById(id);
+        filmToDelete.ifPresent(repository::delete);
         List<Film> showFindByCategory = repository.findByCategory("Drama");
         model.addAttribute("showFindByCategory", showFindByCategory);
         return "showFindByCategory";
