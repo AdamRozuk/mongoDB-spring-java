5768e48f3107ada9748590f7bc90ceefff1de65e
Przemek <staiek199610@wp.pl>
2020-06-16 11:58:38 +0200

    added user class

---
+++ src/main/java/com/example/mongoDB/entities/User.java
@@ -0,0 +1,23 @@
+package com.example.mongoDB.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
+@Getter
+@Setter
+public class User {
+
+    @Id
+    public String nickname;
+    public List<String> LikedFilmsId;
+
+    public User(String nickname) {
+        this.nickname = nickname;
+    }
+
+}
